5e84d56ae2c787cea550b917f2d94c5ddb8be8a2
RAPHAEL DE LEMOS PIRES <raphaellpires@gmail.com>
2026-04-24 10:07:28 -0300

    merge: sincronização projeto local com remoto

---
+++ src/main/java/com/acervo/acervoespirita/AcervoEspiritaApplication.java
@@ -0,0 +1,13 @@
+package com.acervo.acervoespirita;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcervoEspiritaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcervoEspiritaApplication.class, args);
+	}
+
+}
